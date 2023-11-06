e25fee1c2eff67971de42ca24c45dc82c0db207a
Jacob Rogers <rogersair2@aol.com>
2023-11-05 22:19:18 -0500

    aux nav bar fading

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -5,12 +5,10 @@ function toggleHamburgerIcon() {
     if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
         hamburgerIcon.src = 'assets/images/cancel_x_icon.png';
         auxiliaryNav.style.opacity = "1";
+        auxiliaryNav.style.pointerEvents = "auto"; // Make it interactive
     } else {
         hamburgerIcon.src = 'assets/images/hamburgerMenu.png';
         auxiliaryNav.style.opacity = "0";
+        auxiliaryNav.style.pointerEvents = "none"; // Make it non-interactive
     }   
 }
-
-
-
-
