07c29d90dbff897f1c62c6c5af1ea20579d8f84e
Jacob Rogers <rogersair2@aol.com>
2023-11-05 20:08:41 -0500

    removing search for index

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,27 +1,14 @@
 function toggleHamburgerIcon() {
     var hamburgerIcon = document.getElementById('hamburgerMenuIcon');
     var auxiliaryNav = document.getElementById('auxiliaryNav');
-    var url = document.URL;
-    var filename = url.substring(url.lastIndexOf('/')+1);
     
-    if(filename === "index.html"){
-        if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
-            hamburgerIcon.src = 'assets/images/cancel_x_icon.png';
-            auxiliaryNav.style.opacity = "1";
-        } else {
-            hamburgerIcon.src = 'assets/images/hamburgerMenu.png';
-            auxiliaryNav.style.opacity = "0";
-        }
-    }
-    else {
-        if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
-            hamburgerIcon.src = '../assets/images/cancel_x_icon.png';
-            auxiliaryNav.style.opacity = "1";
-        } else {
-            hamburgerIcon.src = '../assets/images/hamburgerMenu.png';
-            auxiliaryNav.style.opacity = "0";
-        }
-    }
+    if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
+        hamburgerIcon.src = 'assets/images/cancel_x_icon.png';
+        auxiliaryNav.style.opacity = "1";
+    } else {
+        hamburgerIcon.src = 'assets/images/hamburgerMenu.png';
+        auxiliaryNav.style.opacity = "0";
+    }   
 }
 
 
