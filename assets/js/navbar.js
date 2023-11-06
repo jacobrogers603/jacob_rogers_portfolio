5cbfeea578fe753d2405c03c53385f262c08f07a
Jacob Rogers <rogersair2@aol.com>
2023-11-05 19:01:07 -0500

    working aux nav bar w/o filtering

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,12 @@
 function toggleHamburgerIcon() {
     var hamburgerIcon = document.getElementById('hamburgerMenuIcon');
+    var auxiliaryNav = document.getElementById('auxiliaryNav');
     if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
         hamburgerIcon.src = 'assets/images/cancel_x_icon.png';
+        auxiliaryNav.style.opacity = "1";
     } else {
         hamburgerIcon.src = 'assets/images/hamburgerMenu.png';
+        auxiliaryNav.style.opacity = "0";
     }
 }
 
