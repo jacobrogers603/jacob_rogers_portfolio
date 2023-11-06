7408a55f85c4c83e1226563b37f96205a6096ea8
Jacob Rogers <rogersair2@aol.com>
2023-11-05 20:01:30 -0500

    make filter appear in place of hamburger

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,29 @@
 function toggleHamburgerIcon() {
     var hamburgerIcon = document.getElementById('hamburgerMenuIcon');
     var auxiliaryNav = document.getElementById('auxiliaryNav');
-    if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
-        hamburgerIcon.src = 'assets/images/cancel_x_icon.png';
-        auxiliaryNav.style.opacity = "1";
-    } else {
-        hamburgerIcon.src = 'assets/images/hamburgerMenu.png';
-        auxiliaryNav.style.opacity = "0";
+    var url = document.URL;
+    var filename = url.substring(url.lastIndexOf('/')+1);
+    
+    if(filename === "index.html"){
+        if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
+            hamburgerIcon.src = 'assets/images/cancel_x_icon.png';
+            auxiliaryNav.style.opacity = "1";
+        } else {
+            hamburgerIcon.src = 'assets/images/hamburgerMenu.png';
+            auxiliaryNav.style.opacity = "0";
+        }
+    }
+    else {
+        if (hamburgerIcon.src.includes('hamburgerMenu.png')) {
+            hamburgerIcon.src = '../assets/images/cancel_x_icon.png';
+            auxiliaryNav.style.opacity = "1";
+        } else {
+            hamburgerIcon.src = '../assets/images/hamburgerMenu.png';
+            auxiliaryNav.style.opacity = "0";
+        }
     }
 }
+
+
+
 
